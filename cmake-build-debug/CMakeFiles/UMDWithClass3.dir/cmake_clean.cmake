5a4dc7c3c9fae17d5f9a20b383703f8aa4873813
TCorn <theo@capek.aero>
2021-05-09 14:52:44 +0200

    compiles

---
+++ cmake-build-debug/CMakeFiles/UMDWithClass3.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UMDWithClass3.dir/AddressIdentifier.cpp.obj"
+  "CMakeFiles/UMDWithClass3.dir/BluetoothSerial.cpp.obj"
+  "CMakeFiles/UMDWithClass3.dir/JsonParserFunctions.cpp.obj"
+  "CMakeFiles/UMDWithClass3.dir/MPU9250.cpp.obj"
+  "CMakeFiles/UMDWithClass3.dir/Sensor.cpp.obj"
+  "CMakeFiles/UMDWithClass3.dir/displayFunctions.cpp.obj"
+  "CMakeFiles/UMDWithClass3.dir/findSensors.cpp.obj"
+  "CMakeFiles/UMDWithClass3.dir/spiffs.cpp.obj"
+  "UMDWithClass3.exe"
+  "UMDWithClass3.exe.manifest"
+  "UMDWithClass3.lib"
+  "UMDWithClass3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UMDWithClass3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
