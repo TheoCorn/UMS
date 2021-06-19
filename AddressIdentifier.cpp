9ce6afe6a7dacce794ea65ce01b192ecdd60c268
TCorn <theo@capek.aero>
2021-06-19 22:22:25 +0200

    add Unknown Sensor as default sensor in getSensorForEnumPos

--- AddressIdentifier.cpp
+++ AddressIdentifier.cpp
@@ -9,6 +9,7 @@
 #include "sensorEnum.h"
 #include "MPU9250.hpp"
 #include <ArduinoJson.h>
+#include "UnknownSensor.h"
 
 
 
@@ -59,7 +60,7 @@ void SensorsIdentifierManager::addSensor(unsigned int enumPos, uint8_t address, 
         case sensorEnum::MPU9250: return (Sensor*)(new class MPU9250(address)); break;
 //        case sensorEnum::BMP280: return (sensor*)(new class BMP280); break;
 
-        default: return nullptr; break;
+        default: return UnknownSensor(address); break;
     }
 }
 
