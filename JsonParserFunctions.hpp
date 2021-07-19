56431f25601ff74bbb8152be1a8d2d92d617bd9f
TCorn <theo@capek.aero>
2021-07-19 12:26:38 +0200

    debuging

--- JsonParserFunctions.hpp
+++ JsonParserFunctions.hpp
@@ -13,7 +13,7 @@ namespace jp {
 
     void parseJson(const char * buffer, std::function<void(JsonDocument*, std::function<JsonPair*>)>& mDo, std::function<void(JsonPair*)>& actualDo);
 
-    void cycleThruObj(JsonDocument * doc, std::function<void(JsonPair*)> actualDo);
+    void cycleThruObj(JsonDocument * doc, std::function<void(JsonPair*)>& actualDo);
 
     void parseJsonWithCycleThru(std::vector<char> * btBuffer, std::function<void(JsonPair*)>& actualDo);
 
