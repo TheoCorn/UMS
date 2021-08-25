6a70f780aa881075e23f70cb55ec8526c24aee50
TCorn <theo@capek.aero>
2021-08-25 12:23:09 +0200

    adding new ui

--- displayFunctions.h
+++ displayFunctions.h
@@ -43,12 +43,12 @@ public:
         display = new Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
         tabIterator = tabs.begin();
 
+        init();
+
         display->clearDisplay();
         (*tabIterator)->render(display, start, end);
         display->display();
 
-
-        init();
     }
 
 
