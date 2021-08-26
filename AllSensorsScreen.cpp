f42316b8ec77d81a68765fec91ce90b43cc5df08
TCorn <theo@capek.aero>
2021-08-26 18:27:34 +0200

    adding new ui

--- AllSensorsScreen.cpp
+++ AllSensorsScreen.cpp
@@ -5,6 +5,9 @@
 #include "AllSensorsScreen.h"
 
 void AllSensorsScreen::render(Adafruit_SSD1306 *display, ui::coordinates &start, ui::coordinates &end) {
+
+    if (sensors->empty()) return
+
     display->setTextColor(SSD1306_BLACK, SSD1306_WHITE);
 
     std::_Rb_tree_iterator<std::pair<const uint32_t, Sensor *>> it = activeSensorIterator;
