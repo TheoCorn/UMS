7c36326b8f5ed3142c157949c795757a1a4e6d80
TCorn <theo@capek.aero>
2021-08-29 21:04:19 +0200

    adding new ui

--- AllSensorsScreen.cpp
+++ AllSensorsScreen.cpp
@@ -48,7 +48,7 @@ void AllSensorsScreen::printSensors(Adafruit_SSD1306 *display,
     display->setTextColor(SSD1306_WHITE);
     
     for (++itStart; itStart != itEnd; ++itStart) {
-        printf("coordinates end x: %d, y%; current cursor y: %d", end.x, end.y, display->getCursorY());
+        printf("coordinates end x: %d, y: %d; current cursor y: %d", end.x, end.y, display->getCursorY());
         if (display->getCursorY() <= end.y) return;
         display->println(itStart->second->getStringForDisplay());
     }
