34875d67ffe4de5a26b3e1bf7c1aab6a6fcd1b3e
TCorn <theo@capek.aero>
2021-09-14 19:57:39 +0200

    adding new ui

--- MPU9250.cpp
+++ MPU9250.cpp
@@ -126,7 +126,7 @@ String MPU9250::getExtendedStringForDisplay() {
     for (int i = 0; i < 10; i++) {
         float f = readFeature(i);
         char cBuffer[64];
-        int ret = sprintf(&cBuffer[0], "%0.2f", f);
+        int ret = sprintf(&cBuffer[0], "%e", f);
         s += mpuFeaturesString[i];
         s += " ";
         s += activeFeaturesVec[i] ? '1' : '0';
