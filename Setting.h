3028f4e2a175f47947a7587aba3e7985f052a1ab
TCorn <theo@capek.aero>
2021-09-20 22:42:34 +0200

    adding new ui

--- Setting.h
+++ Setting.h
@@ -1,9 +1,13 @@
+#include <utility>
+
 //
 // Created by theo on 9/20/2021.
 //
 
 #ifndef UMDWITHCLASS3_SETTING_H
 #define UMDWITHCLASS3_SETTING_H
+
+#include <vector>
 
 
 /**
@@ -15,7 +19,7 @@ struct Setting {
     const char* name;
 
     Setting(const char* name, std::vector<const char *> values, size_t currentIndex) :
-            name(name), values(values), currentIndex(currentIndex) {}
+            name(name), values(std::move(values)), currentIndex(currentIndex) {}
 
     /**
      * if possible decrements index of the setting
