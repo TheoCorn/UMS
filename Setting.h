2173c18eb2d20258a899b891ced15ac74427f12a
TCorn <theo@capek.aero>
2021-09-22 18:57:13 +0200

    adding new ui

--- Setting.h
+++ Setting.h
@@ -33,7 +33,14 @@ struct GeneralSetting {
     inline void onUp() { if (currentIndex != 0) currentIndex--; }
     inline void onDown() { if (currentIndex + 1 < values.size()) currentIndex++; }
 
-    void set() const { onSet(currentIndex); }
+    void set() const {
+
+        Serial.print("index is: ");
+        Serial.println(currentIndex);
+
+        onSet(currentIndex);
+
+    }
 };
 
 
