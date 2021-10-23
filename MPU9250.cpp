74c05b593e5369bef62e88e7690c4badda800d0e
TCorn <theo@capek.aero>
2021-10-23 15:27:53 +0200

    adding sleeping

--- MPU9250.cpp
+++ MPU9250.cpp
@@ -10,11 +10,13 @@ MPU9250::MPU9250(uint8_t address) : Sensor() {
     _i2c = &Wire; // I2C bus
     _address = address; // I2C address
     _useSPI = false; // set to use I2C
-    setUp();
     begin();
 }
 
 void MPU9250::setUp() {
+
+    Sensor::savedSettingsLoader("/sensorData/1.json", )
+
     char *cArrJson = (char *) spiffs::readFile(SPIFFS, "/sensorData/1.json");
 
     JsonDocument *doc = jp::parseJson(cArrJson);
@@ -25,13 +27,19 @@ void MPU9250::setUp() {
         activeFeaturesVec.clear();
         for (JsonVariant v: activeFeatures) {
             activeFeaturesVec.emplace_back(v.as<bool>());
+
         }
+
+
+
+        activeFeaturesVec.shrink_to_fit();
 
         xSettings.clear();
         for (JsonVariant v: locXSettings) {
             unsigned int setting = v.as<unsigned int>();
             xSettings.emplace_back(setting);
         }
+        xSettings.shrink_to_fit();
 
 
     }
