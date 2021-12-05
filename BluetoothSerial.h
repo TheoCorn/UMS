553345b8ac622222aa350c5bf3634b7c9fa2d813
TCorn <theo@capek.aero>
2021-12-05 15:10:50 +0100

    mqtt

--- BluetoothSerial.h
+++ BluetoothSerial.h
@@ -46,7 +46,7 @@ class BluetoothSerial:  public SerialCom
         int peek(void);
         int read(void);
         void read(char * c) override;
-        size_t write(const uint8_t c) override;
+        size_t write(uint8_t c) override;
         size_t write(const uint8_t *buffer, size_t size) override;
         size_t write(JsonDocument * doc) override;
         size_t write(error::Error* error) override;
